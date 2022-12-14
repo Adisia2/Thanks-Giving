34463af9dedaea2a6dcbef567df397060afe1fed
+John <+>
2022-12-14 17:51:00 +0300

    creating crucial files

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // When the page loads, get a list of books from http://localhost:3000/film and display their titles by creating a li for each film and adding each li to the ul#list element.
+    
+    displayMovies();
+});
